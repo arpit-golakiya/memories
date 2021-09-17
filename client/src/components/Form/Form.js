23cf8d5a2cbb88ec7f0fc328ae848f93ea7a6ba8
arpit-golakiya <arpitgolakiya97@gmail.com>
2021-09-17 22:47:37 +0530

    feat: add map to select user (WIP).

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,20 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import {TextField, Button, Typography, Paper, Modal} from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import FileBase from 'react-file-base64';
 import { useHistory } from 'react-router-dom';
 import ChipInput from 'material-ui-chip-input';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { createPost, updatePost } from '../../actions/posts';
 import useStyles from './styles';
+import Map from "../Map/Map";
+
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+
+    return {
+        height:"300px",
+        width:"800px",
+        top: '20px',
+        left: `${50}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStylesMaterial = makeStyles((theme) => ({
+    paper: {
+        position: 'absolute',
+        width: 600,
+        height:400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
 
 const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({ title: '', message: '', tags: [], selectedFile: '' });
   const post = useSelector((state) => (currentId ? state.posts.posts.find((message) => message._id === currentId) : null));
   const dispatch = useDispatch();
   const classes = useStyles();
+  const classesMaterial = useStylesMaterial();
   const user = JSON.parse(localStorage.getItem('profile'));
   const history = useHistory();
+    // getModalStyle is not a pure function, we roll the style only on the first render
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
 
   const clear = () => {
     setCurrentId(0);
@@ -48,6 +83,20 @@ const Form = ({ currentId, setCurrentId }) => {
     );
   }
 
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const body = (
+        <div style={modalStyle} className={classesMaterial.paper}>
+            <Map />
+        </div>
+    );
+
   const handleAddChip = (tag) => {
     setPostData({ ...postData, tags: [...postData.tags, tag] });
   };
@@ -74,6 +123,19 @@ const Form = ({ currentId, setCurrentId }) => {
           />
         </div>
         <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
+          <div>
+              <Button variant="outlined" type="button" onClick={handleOpen}>
+                  Choose Location
+              </Button>
+              <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="simple-modal-title"
+                  aria-describedby="simple-modal-description"
+              >
+                  {body}
+              </Modal>
+          </div>
         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
       </form>
